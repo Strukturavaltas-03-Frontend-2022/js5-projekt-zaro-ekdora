1fff36058f53bba4310de0baf926504fd41a2349
ekdora <dora.egrykiss@gmail.com>
2022-09-20 17:37:58 +0200

    [WIP] refactoring

--- js/modal-handler.js
+++ js/modal-handler.js
@@ -1,28 +1,48 @@
-import { createForm, createModalFooter } from './template-builder.js';
+import { postUser, putUser } from './backend.js';
+import { handleUserChange } from './user-handler.js';
 
 const modal = document.querySelector('.table__modal');
 const closeModalX = document.querySelector('.modal__close-btn');
 const addUser = document.querySelector('.header-action-icon');
+const cancelBtn = document.querySelector('.cancel-btn');
+const submitBtn = document.querySelector('.submit-btn');
+const idInput = document.querySelector('.form__input_id');
+const nameInput = document.querySelector('.form__input_name');
+const emailInput = document.querySelector('.form__input_email');
+const addressInput = document.querySelector('.form__input_address');
 
-addUser.addEventListener('click', () => openModal());
+addUser.addEventListener('click', () => showModal());
+cancelBtn.addEventListener('click', () => hideModal());
+submitBtn.addEventListener('click', () => {
+    const formEntries = getFormEntries();
+    const userPromise = formEntries.id ? putUser(formEntries) : postUser(formEntries);
+    userPromise.then(user => {
+        handleUserChange(user, !formEntries.id);
+        hideModal();
+    });
+});
 
-const openModal = (user = null) => {
-    const modalForm = document.querySelector('.modal__form');
-    const modalFooter = document.querySelector('.modal__footer');
-    if (modalForm) {
-        modalForm.remove();
-        modalFooter.remove();
-    }
-    createForm(user);
-    createModalFooter();
+const openModal = (user) => {
+    fillForm(user);
     showModal();
 };
+
+const fillForm = (user) => {
+    idInput.value = user.id;
+    nameInput.value = user.name;
+    emailInput.value = user.email;
+    addressInput.value = user.address;
+}
 
 const getFormEntries = () => {
     const formData = new FormData(document.userForm);
     const formEntries = {};
     for (const entry of formData.entries()) {
-        formEntries[entry[0]] = entry[1];
+        const key = entry[0];
+        const value = entry[1];
+        if (key !== 'id' || (key === 'id' && value)) {
+            formEntries[key] = value;
+        }
     };
 
     return formEntries;
@@ -48,4 +68,4 @@ window.onclick = function (event) {
 
 window.onresize = hideModal;
 
-export { getFormEntries, openModal, hideModal };
+export { openModal };
--- js/template-builder.js
+++ js/template-builder.js
@@ -1,10 +1,7 @@
-import { getFormEntries, openModal, hideModal } from './modal-handler.js';
-import { postUser, putUser } from './backend.js';
-import { handleUserChange, removeUser } from './user-handler.js';
+import { openModal } from './modal-handler.js';
+import { removeUser } from './user-handler.js';
 
 const tableRows = document.querySelector('.table-rows');
-const modalFormPlaceholder = document.querySelector('.modal__form-placeholder');
-const modalFooterPlaceholder = document.querySelector('.modal__footer-placeholder');
 
 const createUserRow = (user) => {
     const userRow = document.createElement('div');
@@ -51,73 +48,4 @@ const createUserRow = (user) => {
     tableRows.appendChild(userRow);
 };
 
-const createForm = (user = null) => {
-    const form = document.createElement('form');
-    form.classList.add('modal__form', 'flex', 'flex-col', 'items-center', 'gap-4');
-    form.name = 'userForm'
-
-    if (user) {
-        const userId = document.createElement('input');
-        userId.type = 'hidden';
-        userId.name = 'id';
-        userId.value = user.id;
-        form.appendChild(userId);
-    }
-
-    const name = document.createElement('input');
-    name.classList.add('form__input_name', 'form__input');
-    name.type = 'text';
-    name.placeholder = 'Name';
-    name.name = 'name';
-    name.value = user ? user.name : null;
-
-    const email = document.createElement('input');
-    email.classList.add('form__input_email', 'form__input');
-    email.type = 'text';
-    email.placeholder = 'E-Mail';
-    email.name = 'email';
-    email.value = user ? user.email : null;
-
-    const address = document.createElement('input');
-    address.classList.add('form__input_address', 'form__input');
-    address.type = 'text';
-    address.placeholder = 'Address';
-    address.name = 'address';
-    address.value = user ? user.address : null;
-
-    form.appendChild(name);
-    form.appendChild(email);
-    form.appendChild(address);
-
-    modalFormPlaceholder.appendChild(form);
-};
-
-const createModalFooter = () => {
-    const modalFooter = document.createElement('div');
-    modalFooter.classList.add('modal__footer', 'flex', 'justify-end');
-
-    const cancelFormBtn = document.createElement('button');
-    cancelFormBtn.classList.add('cancel-btn', 'modal-btn');
-    cancelFormBtn.innerHTML = 'Cancel';
-    cancelFormBtn.onclick = () => hideModal();
-
-    const submitFormBtn = document.createElement('button');
-    submitFormBtn.classList.add('submit-btn', 'modal-btn');
-    submitFormBtn.innerHTML = 'Submit';
-    submitFormBtn.type = 'submit';
-    submitFormBtn.onclick = () => {
-        const formEntries = getFormEntries();
-        const userPromise = formEntries.id ? putUser(formEntries) : postUser(formEntries);
-        userPromise.then(user => {
-            const { id } = user;
-            handleUserChange({ ...formEntries, id }, !formEntries.id);
-            hideModal();
-        });
-    };
-
-    modalFooter.appendChild(cancelFormBtn);
-    modalFooter.appendChild(submitFormBtn);
-    modalFooterPlaceholder.appendChild(modalFooter);
-};
-
-export { createUserRow, createForm, createModalFooter };
+export { createUserRow };
